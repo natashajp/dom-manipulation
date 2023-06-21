aa8d11ccddb0bb4eff34c881b798645c6e6474d6
natashajp <njperson1219@gmail.com>
2023-06-20 21:02:45 -0400

    Homework completed

--- script.js
+++ script.js
@@ -17,32 +17,70 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+document.getElementById("main-title").innerHTML = "Dom Torretto's Website";
 
   // Part 2
-
+document.querySelector("body").style.backgroundColor = "lightblue"
 
   // Part 3
+const favThings = document.getElementById("favorite-things");
 
+favThings.removeChild(favThings.lastElementChild);
 
   // Part 4
+const specialTitle = document.querySelectorAll(".special-title"); i = specialTitle.length;
 
+while(i--) {
+  specialTitle[i].style.fontSize = "2rem";
+}
 
   // Part 5
+const pastRaces = document.getElementById("past-races");
 
+if (pastRaces.hasChildNodes()) {
+  pastRaces.removeChild(pastRaces.children[3])
+}
 
   // Part 6
-
+const li = document.createElement('li');
+li.textContent = "Baltimore";
+pastRaces.appendChild(li);
 
   // Part 7
+const mainEl = document.querySelector(".main"); 
 
+const div = document.createElement("div");
+div.setAttribute("class", "blog-post");
+div.innerHTML = "<h1 style = 'color: white'>Baltimore</h1> <p style = 'font-size: 1.5rem'>I RACED AROUND THE INNER HARBOR!</p>";
+div.style.backgroundColor = "rebeccapurple";
+mainEl.appendChild(div);
 
   // Part 8
+const quoteTitle = document.querySelector("#quote-title");
 
+quoteTitle.addEventListener("click", randomQuote);
 
   // Part 9
+const blogPost = document.querySelectorAll(".blog-post");
 
+for (let j = 0; j < blogPost.length; j++) {
+  blogPost[j].addEventListener("mouseout", function() {
+    blogPost[j].classList.toggle("purple");
+  })
+  blogPost[j].addEventListener("mouseenter", function() {
+    blogPost[j].classList.toggle("red");
+  })
+}
+ 
+// const blogPost = document.querySelectorAll(".blog-post"); j = blogPost.length;
 
-
+// while(j--) {
+//   blogPost[j].addEventListener("mouseout", function() {
+//     blogPost[j].classList.toggle("purple")
+//   })
+//   blogPost[j].addEventListener("mouseenter", function() {
+//     blogPost[j].classList.toggle("red")
+//   })
+// }
 
 });
